7b2613d7a1c139dd479aa6df868b420c05594862
Lavanya Singh <lavanyasingh2203@gmail.com>
2026-02-01 16:49:55 +0530

    Add files via upload

---
+++ Q1.c
@@ -0,0 +1,43 @@
+//Q1: Insert an Element in an Array
+//Problem: Write a C program to insert an element x at a given 1-based position pos in an array of n integers. Shift existing elements to the right to make space.
+
+#include <stdio.h>
+#define MAX_SIZE 100
+
+int main() {
+    int arr[MAX_SIZE];
+    int n, x, pos, i;
+
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the element to be inserted: ");
+    scanf("%d", &x);
+    printf("Enter the position (1-based index) to insert the element: ");
+    scanf("%d", &pos);
+
+    if (pos < 1 || pos > n + 1) {
+        printf("Invalid position!\n");
+        return 1;
+    }
+
+    for (i = n; i >= pos; i--) {
+        arr[i] = arr[i - 1];
+    }
+
+    arr[pos - 1] = x;
+    n++; // Increase the size of the array
+
+    printf("Array after insertion:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
